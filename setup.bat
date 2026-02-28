6ad1f81fb3c32586a0c02f075fbb05db50eec3a4
bigbroo7 <takundausayiwevu2@gmail.com>
2026-02-28 15:51:12 +0200

    Create setup.bat for Windows setup script

---
+++ setup.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM Windows Setup Script
+
+echo "Setup started at 2026-02-28 13:50:46 UTC"
+
+REM Add your setup commands below:
+echo "Running setup commands..."
+
+REM Sample commands:
+REM mkdir new_folder
+REM copy some_file.txt new_folder\
+echo "Setup completed."
